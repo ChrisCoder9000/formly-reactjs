68b9fd5648108a1e09d026160745c65a5fe50576
Christian <alch.infoemail@gmail.com>
2025-01-30 14:48:34 +0100

    added require('@tailwindcss/jit-plugins/dynamic-classes')

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,5 +76,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/jit-plugins/dynamic-classes"),
+  ],
 };
